302f23d2595ce8685178db67400270c9408335b9
Emmanuel Abia <emmanuel.abia32@gmail.com>
2023-06-10 12:55:37 +0000

    web-debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,7 +1,15 @@
-# puppet script that renames a file
+#Fix the log Errors, catch the error and fix it
+exec {'/etc/php5/apache2/php.ini':
+  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
+  command => "sed -i 's/display_errors = Off/display_errors = On/g' /etc/php5/apache2/php.ini",
+}
 
-exec { 'rename_file':
-  command => 'sudo cp /var/www/html/wp-includes/class-wp-locale.php /var/www/html/wp-includes/class-wp-locale.phpp',
-  path    => '/bin',
-  onlyif  => 'test -f /var/www/html/wp-includes/class-wp-locale.php',
+exec { '/var/www/html/wp-setting.php':
+  path    => [ '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin' ],
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+}
+
+exec {'/etc/init.d/apache2':
+  path    => ['/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'],
+  command => '/etc/init.d/apache2 restart',
 }
